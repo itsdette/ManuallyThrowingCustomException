f29d398e46e3287541d01d89eba37ae48ae04010
itsdette <144678356+itsdette@users.noreply.github.com>
2023-10-17 23:54:52 +0800

    Add files via upload

---
+++ Inventory/Inventory/frmAddProduct.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory
+{
+    public partial class frmAddProduct : Form
+    {
+        private string _ProductName;
+        private string _Category;
+        private string _MfgDate;
+        private string _ExpDate;
+        private string _Description;
+        private int _Quantity;
+        private double _SellPrice;
+        public BindingSource showProductList;
+        public frmAddProduct()
+        {
+            showProductList = new BindingSource();
+            InitializeComponent();
+        }
+        private void frmAddProduct_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProductCategory = new string[]
+            {
+                "Beverages",
+                "Bread/Bakery",
+                "Canned/Jarred Goods",
+                "Dairy",
+                "Frozen Goods",
+                "Meat",
+                "Personal Care",
+                "Other"
+            };
+
+            foreach (string Category in ListOfProductCategory)
+            {
+                cbCategory.Items.Add(Category);
+            }
+        }
+
+        public string Product_Name(string name)
+        {
+            if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+            {
+                throw new StringFormatException("Invalid Product Name. ");
+                //Exception here
+            }
+
+            return name;
+        }
+        public int Quantity(string qty)
+        {
+            if (!Regex.IsMatch(qty, @"^[0-9]"))
+            {
+                throw new NumberFormatException("Invalid Quantity.");
+                //Exception here
+            }
+            return Convert.ToInt32(qty);
+        }
+        public double SellingPrice(string price)
+        {
+            if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
+            {
+                throw new CurrencyFormatException("Invalid Selling Price. ");
+                //Exception here
+            }
+            return Convert.ToDouble(price);
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _ProductName = Product_Name(txtProductName.Text);
+                _Category = cbCategory.Text;
+                _MfgDate = dtPickerMfgDate.Value.ToString("yyyy-MM-dd");
+                _ExpDate = dtPickerExpDate.Value.ToString("yyyy-MM-dd");
+                _Description = richTxtDescription.Text;
+                _Quantity = Quantity(txtQuantity.Text);
+                _SellPrice = SellingPrice(txtSellPrice.Text);
+                showProductList.Add(new ProductClass(_ProductName, _Category, _MfgDate, _ExpDate, _SellPrice, _Quantity, _Description));
+                gridViewProductList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                gridViewProductList.DataSource = showProductList;
+
+                txtProductName.Clear();
+                txtQuantity.Clear();
+                txtSellPrice.Clear();
+                richTxtDescription.Clear();
+            }
+
+            catch (NumberFormatException ex)
+            {
+                MessageBox.Show($"NumberFormatException: {ex.Message}");
+            }
+
+            catch (StringFormatException ex)
+            {
+                MessageBox.Show($"StringFormatException: {ex.Message}");
+            }
+
+            catch (CurrencyFormatException ex)
+            {
+                MessageBox.Show($"CurrencyFormatException: {ex.Message}");
+            }
+        }
+
+        public class NumberFormatException : Exception
+        {
+                public NumberFormatException(string exception) : base(exception) { }S
+            
+        }
+
+        public class StringFormatException : Exception
+        {
+            public StringFormatException(string exception) : base(exception) { }
+
+        }
+
+        public class CurrencyFormatException : Exception
+        {
+            public CurrencyFormatException(string exception) : base(exception) { }
+        }
+    }
+}
